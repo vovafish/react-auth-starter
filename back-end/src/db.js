1c920669996523410da6735c9c6ea03bbf4b5ece
Vladimirs <vladimirribakov123@mail.ru>
2023-01-05 17:58:40 +0000

    setting up react-auth-project

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -1,15 +1,15 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 let client;
 
 export const initializeDbConnection = async () => {
-    client = await MongoClient.connect('mongodb://localhost:27017', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-}
+  client = await MongoClient.connect("mongodb://0.0.0.0:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
 
-export const getDbConnection = dbName => {
-    const db = client.db(dbName);
-    return db;
-}
+export const getDbConnection = (dbName) => {
+  const db = client.db(dbName);
+  return db;
+};
