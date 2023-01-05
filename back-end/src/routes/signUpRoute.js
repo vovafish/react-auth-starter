9543ad1c418c4e84fa7fa6ff3d9cec8babeecbc8
Vladimirs <vladimirribakov123@mail.ru>
2023-01-05 21:29:59 +0000

    adding signup route to the server

---
+++ back-end/src/routes/signUpRoute.js
@@ -0,0 +1,35 @@
+import { getDbConnection } from "../db";
+import bcrypt from "brycpt";
+import jwt from "jsonwebtoken";
+
+export const signUpRoute = {
+  path: "/api/signup",
+  method: "post",
+  handler: async (req, res) => {
+    const { email, password } = req.body;
+
+    const db = getDbConnection("react-auth-db");
+    const user = await db.collection("users").findOne({ email });
+
+    if (user) {
+      res.sendStatus(409);
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const startingInfo = {
+      hairColor: "",
+      favouriteFood: "",
+      bio: "",
+    };
+
+    const result = await db.collection("users").insertOne({
+      email,
+      passwordHash,
+      info: startingInfo,
+      isVerified: false,
+    });
+
+    const { insertdId } = result;
+  },
+};
