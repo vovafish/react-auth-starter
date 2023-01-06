5bc83f43fb4b5d4051da6d02458ec3884dae50d0
Vladimirs <vladimirribakov123@mail.ru>
2023-01-06 17:09:04 +0000

    adding JSON Web Token to the user info page

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,9 +1,6 @@
-import { logInRoute } from './logInRoute';
-import { signUpRoute } from './signUpRoute';
-import { testRoute } from './testRoute';
+import { logInRoute } from "./logInRoute";
+import { signUpRoute } from "./signUpRoute";
+import { testRoute } from "./testRoute";
+import { updateUserInfoRoute } from "./updateUserInfoRoute";
 
-export const routes = [
-    logInRoute,
-    signUpRoute,
-    testRoute,
-];
+export const routes = [logInRoute, signUpRoute, testRoute, updateUserInfoRoute];
--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -22,46 +22,42 @@ export const updateUserInfoRoute = {
 
     const token = authorization.split(" ")[1];
 
-    jwt.verify(
-      token,
-      process.env.JWT_SECRET,
-      (async = async (err, decoded) => {
-        //401 - user is not authenticated
-        if (err)
-          return res.status(401).json({ message: "Unable to verify token" });
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+      //401 - user is not authenticated
+      if (err)
+        return res.status(401).json({ message: "Unable to verify token" });
 
-        const { id } = decoded;
+      const { id } = decoded;
 
-        if (id != userId)
-          return res
-            .status(403)
-            .json({ message: "Not allowed to update that's data" });
+      if (id != userId)
+        return res
+          .status(403)
+          .json({ message: "Not allowed to update that's data" });
 
-        const db = getDbConnection("react-auth-db");
-        const result = await db
-          .collection("users")
-          .findOneAndUpdate(
-            { _id: ObjectID(id) },
-            { $set: { info: updates } },
-            { returnOriginal: false }
-          );
-        //get data from the actul mongo db objcet
-        const { email, isVerified, info } = result.value;
-
-        jwt.sign(
-          { id, email, isVerified, info },
-          process.env.JWT_SECRET,
-          {
-            expiresIn: "2d",
-          },
-          (err, token) => {
-            if (err) {
-              return res.status(200).json(err);
-            }
-            res.status(200).json({ token });
-          }
+      const db = getDbConnection("react-auth-db");
+      const result = await db
+        .collection("users")
+        .findOneAndUpdate(
+          { _id: ObjectID(id) },
+          { $set: { info: updates } },
+          { returnOriginal: false }
         );
-      })
-    );
+      //get data from the actul mongo db objcet
+      const { email, isVerified, info } = result.value;
+
+      jwt.sign(
+        { id, email, isVerified, info },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "2d",
+        },
+        (err, token) => {
+          if (err) {
+            return res.status(200).json(err);
+          }
+          res.status(200).json({ token });
+        }
+      );
+    });
   },
 };
